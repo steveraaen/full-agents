2ac7ae3c945f9eb6f16614cccdedc65805c61495
Steven J Raaen <sraaen@gmail.com>
2022-07-30 18:39:26 -0400

    ...

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get('/api/playeragents', function(req, res) {
   connection.query(`select playerName, agent from meta`, function (error, results, fields) {              
